c906deb1e09f185eccdac8def4c5b7535f007b29
HeroJomba <gadenish3004@mail.ru>
2021-12-15 20:11:24 +0300

    Create class Main

---
+++ src/bsu/rfe/java/group6/lab4/BYKOV/varA/Main.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group6.lab4.BYKOV.varA;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
